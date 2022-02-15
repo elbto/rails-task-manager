602a8225e04ffb4eb2a0b760cb04c701eb438eeb
Toufik El bouamri <elb.toufik@gmail.com>
2022-02-15 15:38:13 +0100

    Challenge back ok without front

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #new
+  get "tasks/new", to: "tasks#new", as: :new_task
+  #create
+  post "tasks", to: "tasks#create"
+  #edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # index
+  get "tasks", to: "tasks#index"
+  #show
+  get "tasks/:id", to: "tasks#show", as: :task
+  #destroy
+  delete "tasks/:id", to: "tasks#destroy"
+
+  # resources :tasks
 end
